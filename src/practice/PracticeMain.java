bdf85335defbd6dfc1b0eb9c5173fc278fa707c9
lakshmikiran <lakshmikiran.nandula@gmail.com>
2015-11-22 08:57:44 -0800

    initial commit

---
+++ src/practice/PracticeMain.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class PracticeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Foo foo = new Foo();
+		foo.setName("Practising GIT");
+		
+		System.out.println("Pracitsing GIT");
+	}
+
+}
